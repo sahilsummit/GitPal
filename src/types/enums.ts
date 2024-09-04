11b01e25402c4e7085cb6021cb6af4326e93408f
sahilsummit <sjaura@thesummitgrp.com>
2024-09-04 12:31:49 +0530

    GitPal added

---
+++ src/types/enums.ts
@@ -0,0 +1,59 @@
+export const DEFAULT_SLEEP_TIME = 2;
+
+export enum COLOR_TYPES {
+  RED = "red",
+  GREEN = "green",
+  BLUE = "blue",
+  YELLOW = "yellow",
+  MAGENTA = "magenta",
+  CYAN = "cyan",
+  WHITE = "white",
+  BLACK = "black",
+  BG_RED = "bgRed",
+}
+
+export enum GRADIENT_TYPES {
+  ATLAS = "atlas",
+  CRISTAL = "cristal",
+  TEEN = "teen",
+  MIND = "mind",
+  MORNING = "morning",
+  VICE = "vice",
+  PASSION = "passion",
+  FRUIT = "fruit",
+  INSTAGRAM = "instagram",
+  RETRO = "retro",
+  SUMMER = "summer",
+  RAINBOW = "rainbow",
+  PASTEL = "pastel",
+}
+
+export enum QUESTION_TYPES {
+  LIST = "list",
+  RAWLIST = "rawlist",
+  EXPAND = "expand",
+  CHECKBOX = "checkbox",
+  CONFIRM = "confirm",
+  INPUT = "input",
+  NUMBER = "number",
+  PASSWORD = "password",
+  EDITOR = "editor",
+}
+
+export const MESSAGES = {
+  WELCOME: "WELCOME  TO  BONGCLOUD",
+  RULER:
+    "____________________________________________________________________________________________________________________ \n ____________________________________________________________________________________________________________________",
+  ASK_NAME: "Your Name:",
+  INTRO: "🚀✨ Say hello to Bongcloud, your ultimate boredom buster! \n",
+  MODULE_SELECTION:
+    "🎁✨ Dive into our delightful menu and pick your favorite treat from the list below! \n",
+  LANGUAGE_SELECTION: "To which language you wanna translate?",
+  TEXT_TO_BE_TRANSLATTED: "Type in what you wanna translate..",
+  TEXT_SIZE_ERROR: "Text size limit error",
+  LOADING: "Loading....",
+  STILL_IN_PROGRESS: "Still in progress... 🙂",
+  INTRO_TYPING_TEST:
+    "Prepare for a fun typing challenge! We're about to give you a random paragraph to type out. Your goal is to match it perfectly. \n100% accuracy is a must. If there's a single typo, you won't be able to submit the test. ❌ \nReady to show off your typing skills? Let's go! ✨ \n",
+};
+
---
+++ src/utilities/inquirer.ts
@@ -0,0 +1,36 @@
+// import inquirer from "inquirer";
+// import { QUESTION_TYPES } from "../types/enums.js";
+
+// export const askQuestion = async (
+//   message: string,
+//   type: QUESTION_TYPES,
+//   choices?: string[],
+//   name: string = "question"
+// ): Promise<any> => {
+//   const answers = await inquirer.prompt({
+//     name: name,
+//     type: type,
+//     message: message,
+//     choices: choices,
+//   });
+//   return answers[name];
+// };
+
+// // import inquirer from "inquirer";
+// // import { QUESTION_TYPES } from "../types/enums";
+
+// // export const askQuestion = async <T = any>(
+// //   message: string,
+// //   type: QUESTION_TYPES,
+// //   choices?: string[],
+// //   name: string = "question"
+// // ): Promise<T> => {
+// //   const answers = await inquirer.prompt({
+// //     name: name,
+// //     type: 'list', 
+// //     message: message,
+// //     choices: choices,
+// //   });
+
+// //   return answers[name];
+// // };
